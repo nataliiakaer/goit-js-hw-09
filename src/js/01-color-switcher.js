58108436b4cfbf0bb3733e837667f6c7c080dcf7
Nataliia Kruhliak <nataliia.kaer@gmail.com>
2023-04-22 06:36:29 +0300

    Task 1 - Color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+// BTN.disabled = false; //Звертаємось до атрибуту, щоб активувати і назад
 
+// if (btn2.disabled === true) {
+//   btn2.disabled= false;
+// }
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const btnEls = document.querySelector('.section_btn_switcher');
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+let timerId = null;
+
+btnStart.addEventListener('click', onButtonStart);
+btnStop.addEventListener('click', onButtonStop);
+
+function onButtonStart() {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  btnStart.toggleAttribute('disabled');
+  btnStop.removeAttribute('disabled');
+}
+
+function onButtonStop() {
+  clearInterval(timerId);
+  btnStop.disabled = true;
+  btnStart.removeAttribute('disabled');
+}
